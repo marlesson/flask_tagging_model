6a84b5c9536e37fb6555868ca6810900ea7c5210
Marlesson <marlessonsa@gmail.com>
2019-02-05 10:54:18 -0200

    add sample and fix json

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from mlflow.pyfunc import load_pyfunc
 from mlflow.utils import get_jsonable_obj
 import pandas as pd
 
-
-
-app = Flask(__name__)
-
-def init():
-    global model
-    model = load_pyfunc("model")
+app      = Flask(__name__)
+model    = load_pyfunc("model")
+features = ["fixed acidity","volatile acidity",
+            "citric acid","residual sugar","chlorides",
+            "free sulfur dioxide", "total sulfur dioxide",
+            "density","pH","sulphates","alcohol"]
 
 @app.route("/predict", methods=['POST'])
 def predict():
   if request.method == 'POST':
+
     try:
         data = request.get_json()
     except ValueError:
         return jsonify("Please enter a json.")
 
-    input_df = pd.DataFrame(data['values'])
+    input_df    = pd.DataFrame(data['instances'])
+    predictions = model.predict(input_df[features]).tolist()
 
-    return jsonify(model.predict(input_df).tolist()) #jsonify(lin_reg.predict(years_of_experience).tolist())
+    return jsonify({"predictions": predictions})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    init()
     app.run(host='0.0.0.0', port=port, debug=False)
