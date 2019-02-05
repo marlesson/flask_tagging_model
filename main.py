f2885c1211014ff6d7559a8c51511f75fd3fbc3b
Marlesson <marlessonsa@gmail.com>
2019-02-04 23:30:09 -0200

    init

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello World</hi>"
+@app.route("/predict", methods=['POST'])
+def predict():
+  if request.method == 'POST':
+    try:
+        data = request.get_json()
+
+    except ValueError:
+        return jsonify("Please enter a json.")
+
+    return jsonify(data) #jsonify(lin_reg.predict(years_of_experience).tolist())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
